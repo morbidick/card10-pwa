b7f050f1f4689b81c267b75bb3ccad6bd4ec9a1d
Lukas Mohrbacher <lukas.mohrbacher@diconium.com>
2019-11-23 01:00:43 +0100

    prod build

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # CARD10 web app
 
+Web app to control your card10 badge via bluetooth. Supports setting your time, led colors and activating the vibration motor.
+
+
+## Development
+
+* install dependencies `npm install`
+* start dev server `npm start`
+* prod build `npm run build`
+
+
+## Links
+
 * [BLE API](https://firmware.card10.badge.events.ccc.de/bluetooth/card10.html)
 * [Issues](https://git.card10.badge.events.ccc.de/card10/firmware/issues?label_name%5B%5D=4-BLE)
 * [Design](https://events.ccc.de/2019/07/09/design-release-chaos-communication-camp-2019/)
 * [card10.svg](https://card10.badge.events.ccc.de/media/card10buttons.svg)
+
+## License
+
+MIT
--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,28 @@
 import serve from 'rollup-plugin-serve';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
+import copy from 'rollup-plugin-copy';
+
+// dev build if watching, prod build if not
+const dev = !!process.env.ROLLUP_WATCH;
 
 export default {
-    input: './src/app.ts',
+	input: './src/app.ts',
 	output: {
 		format: 'esm',
 		file: './dist/app.js',
 		sourcemap: true,
 	},
-    plugins: [
-		serve(),
+	plugins: [
+		dev && serve(),
 		resolve(),
-		typescript(),
-    ]
+		typescript({
+			objectHashIgnoreUnknownHack: true
+		}),
+		copy({
+			targets: [
+				{ src: 'src/index.html', dest: 'dist' },
+			]
+		}),
+	]
 }
