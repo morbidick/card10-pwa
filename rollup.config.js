cf82b4d6f27d310246c35e3e733b71feb382ef73
Lukas Mohrbacher <lukas.mohrbacher@diconium.com>
2019-12-31 15:23:15 +0100

    add file hash to filename

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,32 @@
 import serve from 'rollup-plugin-serve';
+import indexHTML from 'rollup-plugin-index-html';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
+import minifyHTML from 'rollup-plugin-minify-html-literals';
 import { terser } from 'rollup-plugin-terser'
-import copy from 'rollup-plugin-copy';
 
 // dev build if watching, prod build if not
 const dev = process.env.ROLLUP_WATCH;
 
+const filenames = `[name].${dev ? 'dev' : '[hash]'}.js`
+
 export default {
-	input: './src/app.ts',
+	input: './src/index.html',
 	output: {
+		dir: './dist',
 		format: 'esm',
-		file: './dist/app.js',
 		sourcemap: true,
+		entryFileNames: filenames,
+		chunkFileNames: filenames,
 	},
 	plugins: [
 		dev && serve('dist'),
+		indexHTML(),
 		resolve(),
 		typescript({
 			objectHashIgnoreUnknownHack: true
 		}),
+		!dev && minifyHTML(),
 		!dev && terser(),
-		copy({
-			targets: [
-				{ src: 'src/index.html', dest: 'dist' },
-			]
-		}),
-	]
+	],
 }
